179064928e5c1e0b4b3ae7cb329367b5d15e9f34
dimartvil <dimartvil@hotmail.com>
2022-06-07 22:47:05 -0500

    update

--- src/public/main.js
+++ src/public/main.js
@@ -1,8 +1,36 @@
 const socket = io();
 
+Notification.requestPermission().then(function (result) {
+  console.log(result);
+});
+
+function notifyMe(message) {
+  // Let's check if the browser supports notifications
+  if (!("Notification" in window)) {
+    alert("This browser does not support desktop notification");
+  }
+
+  // Let's check whether notification permissions have already been granted
+  else if (Notification.permission === "granted") {
+    // If it's okay let's create a notification
+    var notification = new Notification(message);
+  }
+
+  // Otherwise, we need to ask the user for permission
+  else if (Notification.permission !== "denied") {
+    Notification.requestPermission(function (permission) {
+      // If the user accepts, let's create a notification
+      if (permission === "granted") {
+        var notification = new Notification(message);
+      }
+    });
+  }
+}
+
 
 socket.on("new message", (data) => {
-  console.log('New SMS');
+  notifyMe("New Message");
+  console.log('New Message');
   const messagesList = document.getElementById("messages");
 
   const li = document.createElement("li");
@@ -18,7 +46,7 @@ socket.on("new message", (data) => {
   //_id.appendChild(document.createTextNode(data._id));
 
   const createdAt = document.createElement('span');
-  createdAt.appendChild(document.createTextNode(data.createdAt));
+  createdAt.appendChild(document.createTextNode(timeago.format(data.createdAt)));
 
   li.appendChild(body);
   li.appendChild(from);
